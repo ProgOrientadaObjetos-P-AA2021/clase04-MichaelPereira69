4234577eb41e5c0dca8993891a364eb866a47faa
Michael Pereira <michaelmaldonadopereira69@gmail.com>
2021-04-28 17:28:46 -0500

    Tabajo en clase

--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete1/Ejecutor.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete1/Ejecutor.java
@@ -13,6 +13,20 @@ public class Ejecutor {
     public static void main(String[] args) {
         
         /*Crear un objeto y presentar los datos*/
-        
+        Hospital h1 = new Hospital ();
+        System.out.printf("%s\n", h1);
+        System.out.println("Los Datos del ospital son:\n");
+        System.out.printf("El nombre del Hospital es: %s\n"
+                + "La Ciudad es: %s\n"
+                + "El Numero de Doctores es: %s\n"
+                + "El Numero de Enfermos es: %s\n",
+                h1.obtenerNombre(),
+                h1.obtenerCiudad(),
+                h1.obtenerNumeroDoctores(),
+                h1.obtenerNumeroEnfermeros());
+        String nombre = "Isidro ayora";
+        String ciudad = "Loja";
+        double numeroDoctores = 100;
+        double NumeroEnfermos = 200;
     }
 }
--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete1/Hospital.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete1/Hospital.java
@@ -10,52 +10,55 @@ package paquete1;
  * @author reroes
  */
 public class Hospital {
+
     private String nombre;
     private String ciudad;
     private int numeroDoctores;
     private int numeroEnfermeros;
-    
-    /*
-        Crear un constructor que no tenga parámetros
+
+    /*  Crear un constructor que no tenga parámetros
         y que asigne valores ya establecidos a todos 
-        los atributos.
-    nombre: Isidro Ayora
-    ciudad: Loja
-    numeroDoctores: 100
-    numeroEnfermeros: 200
-    
-    */
-    
-    public void establecerNombre(String n){
+        los atributos.*/
+        public Hospital(){
+        nombre = "Isidro Ayora";
+        ciudad = "Loja";
+        numeroDoctores = 100;
+        numeroEnfermeros = 200;
+    }
+
+    public void establecerNombre(String n) {
         nombre = n;
     }
-    
-    public void establecerCiudad(String n){
+
+    public void establecerCiudad(String n) {
         ciudad = n;
     }
-    
-    public void establecerNumeroDoctores(int n){
+
+    public void establecerNumeroDoctores(int n) {
         numeroDoctores = n;
     }
-    
-    public void establecerNumeroEnfermeros(int n){
+
+    public void establecerNumeroEnfermeros(int n) {
         numeroEnfermeros = n;
     }
-    
-    public String obtenerNombre(){
+
+    public String obtenerNombre() {
         return nombre;
     }
-    
-    public String obtenerCiudad(){
+
+    public String obtenerCiudad() {
         return ciudad;
     }
-    
-    public int obtenerNumeroDoctores(){
+
+    public int obtenerNumeroDoctores() {
         return numeroDoctores;
     }
-    
-    public int obtenerNumeroEnfermeros(){
+
+    public int obtenerNumeroEnfermeros() {
         return numeroEnfermeros;
     }
-    
+       public int obtenerValorHospital() {
+        return numeroEnfermeros;
+    }
+
 }
